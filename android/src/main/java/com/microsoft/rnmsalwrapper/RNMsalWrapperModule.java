9126557d43754bb5efd9e8ab53d4ea782eb9e596
Ryan Rife <ryrife@microsoft.com>
2020-01-22 16:25:30 -0800

    working android auth

--- android/src/main/java/com/microsoft/rnmsalwrapper/RNMsalWrapperModule.java
+++ android/src/main/java/com/microsoft/rnmsalwrapper/RNMsalWrapperModule.java
@@ -2,6 +2,12 @@
 package com.microsoft.rnmsalwrapper;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Base64;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -9,9 +15,13 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
 import com.microsoft.identity.client.*;
 import com.microsoft.identity.client.exception.*;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 public class RNMsalWrapperModule extends ReactContextBaseJavaModule {
@@ -29,40 +39,29 @@ public class RNMsalWrapperModule extends ReactContextBaseJavaModule {
   }
 
   public interface applicationCallback {
-      void onSuccess(IPublicClientApplication application);
+      void onSuccess(IMultipleAccountPublicClientApplication application);
       void onError(MsalException error);
   }
 
   @ReactMethod
-  public void acquireToken(ReadableMap options, Callback successCallback) {
-      Log.d("msal","acquire token");
+  public void acquireToken(final ReadableMap options, final Callback successCallback) {
     final ArrayList<Object> scopeObjectList = options.hasKey("scopes") ? options.getArray("scopes").toArrayList() : null;
     final String[] scopes = objectArrayListToStringArray(scopeObjectList);
     final String clientId = options.hasKey("clientId") ? options.getString("clientId") : null;
+    final String redirectUri = options.hasKey("redirectUri") ? options.getString("clientId")
+            : buildRedirectUri().toString();
     final Activity currentActivity = this.getReactApplicationContext().getCurrentActivity();
 
-    Log.d("msal", clientId);
-
-    getApplication(clientId, new applicationCallback() {
+    getApplication(clientId, redirectUri, new applicationCallback() {
         @Override
-        public void onSuccess(IPublicClientApplication application) {
-            Log.d("msal","acq_token");
-            application.acquireToken(currentActivity, scopes, new AuthenticationCallback() {
-                @Override
-                public void onCancel() {
+        public void onSuccess(IMultipleAccountPublicClientApplication application) {
+            AcquireTokenParameters interactiveParameters = new AcquireTokenParameters.Builder()
+                    .startAuthorizationFromActivity(getCurrentActivity())
+                    .withScopes(Arrays.asList(scopes))
+                    .withCallback(getAuthenticationCallback(successCallback))
+                    .build();
 
-                }
-
-                @Override
-                public void onSuccess(IAuthenticationResult authenticationResult) {
-                    Log.d("msal","acq_suc");
-                }
-
-                @Override
-                public void onError(MsalException exception) {
-                    Log.d("msal","acq_err");
-                }
-            });
+            application.acquireToken(interactiveParameters);
         }
 
         @Override
@@ -74,16 +73,42 @@ public class RNMsalWrapperModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void acquireTokenSilent(ReadableMap options, Callback successCallback) {
-      ArrayList<Object> scopeObjectList = options.hasKey("scopes") ? options.getArray("scopes").toArrayList() : null;
-      String[] scopes = objectArrayListToStringArray(scopeObjectList);
-      String clientId = options.hasKey("clientId") ? options.getString("clientId") : null;
-      String accountId = options.hasKey("accountId") ? options.getString("accountId") : null;
+  public void acquireTokenSilent(final ReadableMap options, final Callback successCallback) {
+      final ArrayList<Object> scopeObjectList = options.hasKey("scopes") ? options.getArray("scopes").toArrayList() : null;
+      final String[] scopes = objectArrayListToStringArray(scopeObjectList);
+      final String clientId = options.hasKey("clientId") ? options.getString("clientId") : null;
+      final String accountId = options.hasKey("accountId") ? options.getString("accountId") : null;
+      final String redirectUri = options.hasKey("redirectUri") ? options.getString("clientId")
+              : buildRedirectUri().toString();
 
-      getApplication(clientId, new applicationCallback() {
+      getApplication(clientId, redirectUri, new applicationCallback() {
           @Override
-          public void onSuccess(IPublicClientApplication application) {
+          public void onSuccess(final IMultipleAccountPublicClientApplication application) {
+            if(application != null) {
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            IAccount account = application.getAccount(accountId);
 
+                            AcquireTokenSilentParameters silentParameters = new AcquireTokenSilentParameters.Builder()
+                                    .forAccount(account)
+                                    .fromAuthority(application.getConfiguration().getDefaultAuthority().getAuthorityUri().toString())
+                                    .withScopes(Arrays.asList(scopes))
+                                    .withCallback(getAuthenticationCallback(successCallback))
+                                    .build();
+
+                            application.acquireTokenSilentAsync(silentParameters);
+                        } catch (MsalException ex) {
+
+                        } catch (InterruptedException ex) {
+
+                        }
+                    }
+                });
+
+
+            }
           }
 
           @Override
@@ -95,9 +120,50 @@ public class RNMsalWrapperModule extends ReactContextBaseJavaModule {
 
   }
 
+    private AuthenticationCallback getAuthenticationCallback(final Callback successCallback) {
+        return new AuthenticationCallback() {
+            @Override
+            public void onSuccess(IAuthenticationResult authenticationResult) {
+                if(authenticationResult != null && authenticationResult.getAccessToken() != null) {
+
+                    final IAccount cachedAccount = authenticationResult.getAccount();
+
+                    WritableMap response = new WritableNativeMap();
+
+                    response.putString("accessToken", authenticationResult.getAccessToken());
+                    response.putString("accountId", cachedAccount.getId());
+                    response.putString("userName", cachedAccount.getUsername());
+
+                    WritableMap accountDetails = new WritableNativeMap();
+                    Map<String, ?> claims = cachedAccount.getClaims();
+                    for(String claimName: claims.keySet()) {
+                        if(claims.get(claimName) instanceof String) {
+                            accountDetails.putString(claimName, ((String) claims.get(claimName)));
+                        }
+                    }
+                    response.putMap("accountDetails", accountDetails);
+
+                    successCallback.invoke(null, response);
+                }
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+
+            }
+
+            @Override
+            public void onCancel() {
+                // User cancelled the flow
+            }
+        };
+    }
+
   @ReactMethod
   public void removeAccount(ReadableMap options, Callback successCallback) {
 
+      /// do STUFF
+    successCallback.invoke();
   }
 
   private String[] objectArrayListToStringArray(ArrayList<Object> objectList) {
@@ -112,20 +178,47 @@ public class RNMsalWrapperModule extends ReactContextBaseJavaModule {
       return items;
   }
 
-  private void getApplication(final String clientId, final applicationCallback callback) {
-      Log.d("msal","getapp " + clientId);
+  private Uri buildRedirectUri() {
+      final String packageName = this.getReactApplicationContext().getPackageName();
+      final Uri.Builder builder = new Uri.Builder();
+      final Uri uri = builder.scheme("msauth")
+              .authority(packageName)
+              .appendPath(getSignatureHash())
+              .build();
+      return uri;
+  }
 
-      PublicClientApplication.create(this.getReactApplicationContext(), clientId,
+  private String getSignatureHash() {
+      final String packageName = this.getReactApplicationContext().getPackageName();
+      try {
+          final PackageInfo info = this.getReactApplicationContext().getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
+          if(info.signatures.length > 0) {
+              Signature sig = info.signatures[0];
+              final MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+              messageDigest.update(sig.toByteArray());
+              final String signatureHash = android.util.Base64.encodeToString(messageDigest.digest(), Base64.NO_WRAP);
+              return signatureHash;
+          }
+      } catch (PackageManager.NameNotFoundException nnfe) {
+
+      } catch (NoSuchAlgorithmException nsae) {
+
+      }
+
+      return "";
+  }
+
+  private void getApplication(final String clientId, final String redirectUri, final applicationCallback callback) {
+      MultipleAccountPublicClientApplication.create(this.getReactApplicationContext(), clientId, null, redirectUri,
               new IPublicClientApplication.ApplicationCreatedListener() {
                   @Override
                   public void onCreated(IPublicClientApplication application) {
-                      Log.d("msal","getapp_suc");
-                      callback.onSuccess(application);
+                      callback.onSuccess((IMultipleAccountPublicClientApplication) application);
                   }
 
                   @Override
-                  public void onError(MsalException exception) {
-                      Log.d("msal","getapp_err"); callback.onError(exception);
+                  public void onError(MsalException ex) {
+                      callback.onError(ex);
                   }
               }
       );
